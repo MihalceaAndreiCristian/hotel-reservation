5237b2511a811c4e9a0fbc776e1938e8ac2b8508
andreimihalcea <andreicristian.mihalcea@gmail.com>
2025-04-27 19:12:36 +0300

    added form for new reservations and start building the reservations list

---
+++ src/app/reservation/reservation.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import {Reservation} from '../models/reservation';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReservationService {
+
+  private reservations: Reservation[] =[];
+
+  constructor() {
+  let savedReservations = localStorage.getItem('reservations');
+  this.reservations = savedReservations? JSON.parse(savedReservations) : [];
+  }
+  getReservations(): Reservation[] {
+
+    return this.reservations;
+  }
+
+  getReservation(id: string): Reservation | undefined {
+    return this.reservations.find(r => r.id === id);
+  }
+
+  addReservation(reservation: Reservation) {
+    this.reservations.push(reservation);
+
+    localStorage.setItem('reservations', JSON.stringify(this.reservations));
+  }
+
+  deleteReservation(id: string) {
+    let index = this.reservations.findIndex(r => r.id === id);
+    this.reservations.splice(index, 1);
+    localStorage.setItem('reservations', JSON.stringify(this.reservations));
+
+  }
+
+  updateReservation(reservation: Reservation) {
+    let index = this.reservations.findIndex(r => r.id === reservation.id);
+    this.reservations[index] = reservation;
+    localStorage.setItem('reservations', JSON.stringify(this.reservations));
+
+  }
+}
